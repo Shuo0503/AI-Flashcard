6e67b8a9d9c57307828538bf0344e9b9d55cd063
Shuo Li <shuoli0503@gmail.com>
2024-08-15 11:20:07 -0700

    set up and running

---
+++ firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCFjuH3rEX6ZLs_yhATiRA-erEeITyDExs",
+  authDomain: "ai-flashcard-cc461.firebaseapp.com",
+  projectId: "ai-flashcard-cc461",
+  storageBucket: "ai-flashcard-cc461.appspot.com",
+  messagingSenderId: "275358273363",
+  appId: "1:275358273363:web:932581f01ed8d0f8475ba6",
+  measurementId: "G-XR89QZ0E8J"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+export default db;
